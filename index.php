308b223ff4c8760c8a37b545d901da2b77eef2a6
Staale Andre <staaleas@gmail.com>
2015-10-19 17:20:20 +0200

    Lager functions.php fil

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-$context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
-$url = "http://www.yr.no/sted/Norge/Nord-Tr%C3%B8ndelag/N%C3%A6r%C3%B8y/Kolvereid/varsel.xml";
-$xml = file_get_contents($url, false, $context);
-$xml = simplexml_load_string($xml);
-
-//$json = json_encode($xml); //Gjør om XML til json
-//$array = json_decode($json,TRUE); //Gjør om json til array
-
-echo $xml;
+Hello world
 
 ?>
