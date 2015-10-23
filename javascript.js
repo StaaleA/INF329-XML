19dadf4071ce19afb5aa498ab6fc53a71564b9a8
christre42 <christian.rennemo@gmail.com>
2015-10-23 11:25:35 +0200

    Opprettet egen branch for søkemotor

--- javascript.js
+++ javascript.js
@@ -6,13 +6,20 @@
 
 function lesSteder() {
 	var leseFil = new XMLHttpRequest();
-	leseFil.open("GET", "noreg.txt", true);
+	alert("hei");
+	leseFil.open("GET", "/noreg.txt", false);
+	alert("hei igjen");
 	leseFil.onreadystatechange = function() {
+		alert("inne i funksjonen");
 		if(leseFil.readyState === 4) {
+			alert("readyState er lik 4.");
 			if(leseFil.status === 200 || leseFil.status == 0) {
+				alert("status er 200 eller 0, og det bør bli skrevet ut");
 				var lestTekst = leseFil.responseText;
 				document.getElementById("ut").innerHTML = lestTekst;
 			}
 		}
 	} 
+	leseFil.send(null);
+	alert("siste");
 }
