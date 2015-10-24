b0d4c792af35f78a0c2499155418a949274b49b5
Staale Andre <staaleas@gmail.com>
2015-10-24 16:56:45 +0200

    Statisk bilde fra Google Maps OK

--- functions.php
+++ functions.php
@@ -15,7 +15,8 @@ $url = "http://openwps.statkart.no/skwms1/wps.elevation?request=Execute&service=
 $xmlKartverket = simplexml_load_file($url);
 	if ($xmlKartverket->xpath('//wps:ExecuteResponse/wps:ProcessOutputs')) { //Sjekker at  det finnes en ProcessOutputs.  
 		$output = $xmlKartverket->xpath('//wps:ExecuteResponse/wps:ProcessOutputs')[0]; 
-        $elivation = round((float)$output->xpath('wps:Output[ows:Identifier/text()="elevation"]/wps:Data/wps:LiteralData')[0]);
+        $elivation = (float)$output->xpath('wps:Output[ows:Identifier/text()="elevation"]/wps:Data/wps:LiteralData')[0];
+        //$ssrid     = (int)$output->xpath('wps:Output[ows:Identifier/text()="ssrid"]/wps:Data/wps:LiteralData')[0];
     } 
     	else { echo "Noe gikk galt med følgende url: " + $url; };
 
@@ -23,11 +24,39 @@ $xmlKartverket = simplexml_load_file($url);
 $locationNode = $xml->location[0]; //Finner noden "Location" i XML-dokumentet fra YR
 $locationNode->addChild('elevation',$elivation ); //Legger til en node under Location
 
+
+//Faktaark om sted fra KARTVERKET
+//$url = "http://faktaark.statkart.no/SSRFakta/faktaarkfraobjektid?enhet=".$ssrid."&format=xml";
+//$xmlFaktaarkSted = simplexml_load_file($url);
+
+
 //$xml->asXML('varsel.xml'); //Lagre objektet som XML
 return $xml->asXML(); //Returnerer en velformet XML string basert på SimpleXML elementet
 
 }
-	
+
+function getGoogleStaticMap($lat,$long){
+return "http://maps.google.com/maps/api/staticmap?center=".$lat.",".$long."&zoom=14&size=300x200&maptype=roadmap&sensor=false";
+}
+
+function getGoogleMapLink($lat,$long){
+$url = "http://maps.google.com/?q=".$lat.",".$long;
+return $url;
+}
+
+
+function getNorgeskart($lat,$long){
+$kart = "http://www.norgeskart.no/statisk.html#12/".$lat."/".$long."/+embedMaskLayer/+embed.box";
+$url = "http://www.norgeskart.no/#12/".$lat."/".$long;
+$iframe = "<a href='".$url ."'><iframe src='".$kart."' width='300' height='200' border='0'></iframe></a>";
+return $iframe;
+}
+
+$GoogleStaticMap = getGoogleStaticMap("64.8654946456349","11.6046459447957");
+$GoogleMapLink = getGoogleMapLink("64.8654946456349","11.6046459447957");
+
+echo "<a href='".$GoogleMapLink ."'><img src='".$GoogleStaticMap."'></a>";	
+echo getNorgeskart("64.8654946456349","11.6046459447957");
 print_r(parseXML("http://www.yr.no/sted/Norge/Nord-Tr%C3%B8ndelag/N%C3%A6r%C3%B8y/Kolvereid/varsel.xml"));
 
 ?>
