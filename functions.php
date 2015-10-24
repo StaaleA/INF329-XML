cbf9e65603746957f34a454fad0e9d052305a453
Staale Andre <staaleas@gmail.com>
2015-10-24 12:08:15 +0200

    Henter inn XML og legger til node

--- functions.php
+++ functions.php
@@ -1,13 +1,29 @@
 <?php
 
+function parseXML($url){
+
 $context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
-$url = "http://www.yr.no/sted/Norge/Nord-Tr%C3%B8ndelag/N%C3%A6r%C3%B8y/Kolvereid/varsel.xml";
 $xml = file_get_contents($url, false, $context);
 $xml = simplexml_load_string($xml);
 
-//$json = json_encode($xml); //Gjør om XML til json
-//$array = json_decode($json,TRUE); //Gjør om json til array
+//Hente ut lon og lat
+$locationArray = $xml->xpath('location'); //Finner node "Location" Return:array
+echo $xml->count();
+print_r($locationArray);
 
-echo $xml;
+foreach($locationArray as $res) {
+	$latitude= $res['latitude'];
+    $longitude = $res['longitude'];
+}
+
+//Bearbeiding av XML-dokumentet
+$locationNode = $xml->location[0]; //Finner noden "Location" i XML-dokumentet fra YR
+$elevation = $locationNode->addChild('elevation','567'); //Legger til en node under Location
+$xml->asXML('varsel.xml'); //Lagre objektet som XML
+
+
+}
+	
+parseXML("http://www.yr.no/sted/Norge/Nord-Tr%C3%B8ndelag/N%C3%A6r%C3%B8y/Kolvereid/varsel.xml");
 
 ?>
