d0865ef3695a172e0d4526a196e60cbd3c29dc97
Staale Andre <staaleas@gmail.com>
2015-10-24 15:53:42 +0200

    Fjerner diverse uttak
    Tror ikke vi får bruk for stedsnavn, ssrid og terreng, så fjerner
    if-testen og legger til round() direkte. Funksjonen parseXML()
    returnerer nå en XML-streng.

--- functions.php
+++ functions.php
@@ -13,30 +13,21 @@ $longitude = $location["longitude"];
 //Henter elevation fra kartverket basert på lat og long
 $url = "http://openwps.statkart.no/skwms1/wps.elevation?request=Execute&service=WPS&version=1.0.0&identifier=elevation&datainputs=[lat=".$latitude.";lon=".$longitude.";epsg=4326]";
 $xmlKartverket = simplexml_load_file($url);
-	if ($xmlKartverket->xpath('//wps:ExecuteResponse/wps:ProcessOutputs')) {          
+	if ($xmlKartverket->xpath('//wps:ExecuteResponse/wps:ProcessOutputs')) { //Sjekker at  det finnes en ProcessOutputs.  
 		$output = $xmlKartverket->xpath('//wps:ExecuteResponse/wps:ProcessOutputs')[0]; 
-        $elivation   = (float)$output->xpath('wps:Output[ows:Identifier/text()="elevation"]/wps:Data/wps:LiteralData')[0];
-		if ($elivation !== 'nan') { //Sjekker at $elevation har fått en tallverdi
-            $elivation  = round($elivation);  
-            $stedsnavn= (string)$output->xpath('wps:Output[ows:Identifier/text()="placename"]/wps:Data/wps:LiteralData')[0];
-            //$ssrid     = (int)$output->xpath('wps:Output[ows:Identifier/text()="ssrid"]/wps:Data/wps:LiteralData')[0];
-            $terreng  = (string)$output->xpath('wps:Output[ows:Identifier/text()="terrain"]/wps:Data/wps:LiteralData')[0];
-            echo "$stedsnavn $elivation $terreng";
-        }
+        $elivation = round((float)$output->xpath('wps:Output[ows:Identifier/text()="elevation"]/wps:Data/wps:LiteralData')[0]);
     } 
     	else { echo "Noe gikk galt med følgende url: " + $url; };
-
-
-
 
 //Bearbeiding av XML-dokumentet
 $locationNode = $xml->location[0]; //Finner noden "Location" i XML-dokumentet fra YR
 $locationNode->addChild('elevation',$elivation ); //Legger til en node under Location
-$xml->asXML('varsel.xml'); //Lagre objektet som XML
 
+//$xml->asXML('varsel.xml'); //Lagre objektet som XML
+return $xml->asXML(); //Returnerer en velformet XML string basert på SimpleXML elementet
 
 }
 	
-parseXML("http://www.yr.no/sted/Norge/Nord-Tr%C3%B8ndelag/N%C3%A6r%C3%B8y/Kolvereid/varsel.xml");
+print_r(parseXML("http://www.yr.no/sted/Norge/Nord-Tr%C3%B8ndelag/N%C3%A6r%C3%B8y/Kolvereid/varsel.xml"));
 
 ?>
