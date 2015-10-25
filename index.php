5c8c49f075248de965cbc50e206fc325681dd1db
christre42 <christian.rennemo@gmail.com>
2015-10-25 11:38:25 +0100

    Henter ut sted og url fra txt-fil og legger i liste

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-<script type="text/javascript" src="javascript.js"></script>
+<!-- <script type="text/javascript" src="javascript.js"></script> -->
 <title>Hello</title>
 </head>
 <body onload="lesSteder()">
 <h1>Hei</h1>
 <div>Hello</div>
 <div id="ut"></div>
+<div>
+<?php include("readfile.php");?>
+</div>
 </body>
 </html>
--- readfile.php
+++ readfile.php
@@ -1,17 +1,26 @@
 <?php
-$fp = fopen('mydata.csv', 'r');
-
-while ( !feof($fp) )
-{
-    $line = fgets($fp, 2048);
-
-    $someCondition = someConditionToDetermineTabOrComma();
-
-    $delimiter = $someCondition ? "," : "\t";
-    $data = str_getcsv($line, $delimiter);
-
-    doSomethingWithData($data);
-}                              
-
-fclose($fp);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$row = 0;
+$stedListe = array();
+if (($leseFil = fopen("noreg.txt", "r")) !== FALSE) {
+    while (($innData = fgetcsv($leseFil, 0, "	")) !== FALSE) {
+        $num = count($innData);
+        $row++;
+        for ($i=0; $i < $num; $i++) {
+            if($row > 1) {
+	        	$sted = $innData[1];
+	            $url = $innData[12];
+	            $stedListe[$innData[1]] = $innData[12];
+        	} // end if
+        } // next 
+    } // next
+    fclose($leseFil);
+} // end if
+// foreach($stedListe as $x => $x_value) {
+//     echo "Sted=" . $x . ", URL=" . $x_value;
+//     echo "<br>";
+// }
 ?>
+
